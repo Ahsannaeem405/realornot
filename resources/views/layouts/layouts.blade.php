bcb8fda80beba7fd5adff14c3bd37a59cabfe9ec
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2021-09-10 19:21:59 +0500

    Update layouts.blade.php

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Photosen &mdash; Colorlib Website Template</title>
+  <title>Real Or Not</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -34,7 +34,7 @@
 
 
 
-@yield('content');
+@yield('content')
 
     <script src="{{asset('/assets/js/jquery-3.3.1.min.js')}}"></script>
     <script src="{{asset('/assets/js/jquery-migrate-3.0.1.min.js')}}"></script>
